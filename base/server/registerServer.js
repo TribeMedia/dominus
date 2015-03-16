276b9361d40caac9f4aef50b261cf3d9537fb6af
dan <danphi@gmail.com>
2015-03-16 12:48:50 -0700

    register server with home base

--- base/server/registerServer.js
+++ base/server/registerServer.js
@@ -1,10 +1,50 @@
+var os = Npm.require('os');
+var landingConn = DDP.connect(process.env.DOMINUS_BASE);
+
 // register server and game with dominus base
 // dominus_key makes sure it's one of my servers
-if (process.env.DOMINUS_BASE && process.env.GAME_ID && process.env.DOMINUS_KEY) {
-    var landingConn = DDP.connect(process.env.DOMINUS_BASE);
+Meteor.startup(function() {
+    if (process.env.DOMINUS_BASE && process.env.GAME_ID && process.env.DOMINUS_KEY) {
+        registerWithServer();
+        Meteor.setTimeout(function() {
+            registerWithServer();
+        }, 1000*60*10);
+    }
+});
 
-    landingConn.onReconnect = function() {
-        console.log('--- registering with home base')
-        landingConn.call('registerServer', process.env.GAME_ID, process.env.BRANCH_ID, process.env.DOMINUS_WORKER, process.env.DOMINUS_KEY);
-    };
-}
+var registerWithServer = function() {
+    console.log('--- registering with home base');
+    console.log(getIp());
+    landingConn.call(
+        'registerServer',
+        process.env.GAME_ID,
+        process.env.BRANCH_ID,
+        process.env.DOMINUS_WORKER,
+        process.env.DOMINUS_KEY,
+        getIp(),
+        os.uptime(),
+        os.loadavg(),
+        os.totalmem(),
+        os.freemem(),
+        os.cpus()
+    );
+};
+
+
+var getIp = function() {
+    // Get interfaces
+    var netInterfaces = os.networkInterfaces();
+    // Result
+    var result = [];
+    for (var id in netInterfaces) {
+        var netFace = netInterfaces[id];
+
+        for (var i = 0; i < netFace.length; i++) {
+            var ip = netFace[i];
+            if (ip.internal === false && ip.family === 'IPv4') {
+                result.push(ip);
+            }
+        }
+    }
+    return result;
+};
