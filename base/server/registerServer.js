9c5df8d399477250898c975e1a64fc1a1ad00449
dan <danphi@gmail.com>
2015-03-15 23:19:33 -0700

    test

--- base/server/registerServer.js
+++ base/server/registerServer.js
@@ -4,6 +4,7 @@ if (process.env.DOMINUS_BASE && process.env.GAME_ID && process.env.DOMINUS_KEY) 
     var landingConn = DDP.connect(process.env.DOMINUS_BASE);
 
     landingConn.onReconnect = function() {
+        console.log('--- registering with home base')
         landingConn.call('registerServer', process.env.GAME_ID, process.env.BRANCH_ID, process.env.DOMINUS_WORKER, process.env.DOMINUS_KEY);
     };
 }
