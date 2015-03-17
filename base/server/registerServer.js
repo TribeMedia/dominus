bdba41f0c5f2883d910b213a9d9e276b3db8da6d
dan <danphi@gmail.com>
2015-03-17 00:29:14 -0700

    register serve

--- base/server/registerServer.js
+++ base/server/registerServer.js
@@ -5,15 +5,17 @@ landingConn = null;
 // dominus_key makes sure it's one of my servers
 Meteor.startup(function() {
     if (process.env.DOMINUS_BASE && process.env.GAME_ID && process.env.DOMINUS_KEY) {
+
         landingConn = DDP.connect(process.env.DOMINUS_BASE);
-        registerWithServer();
+
+        registerServer();
         Meteor.setInterval(function() {
-            registerWithServer();
+            registerServer();
         }, 1000*60*10);
     }
 });
 
-var registerWithServer = function() {
+var registerServer = function() {
     console.log('--- registering server with '+process.env.DOMINUS_BASE);
 
     // TODO: find a better way to get the ip of the host from inside a docker container
@@ -43,7 +45,7 @@ var registerWithServer = function() {
 
             } else {
                 console.error('not connected to home base');
-                landingConn.reconnect();
+                console.error(landingConn.status());
             }
         }
     });
