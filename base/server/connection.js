46c42751273c170081dfa79c1b03b504edd5d1ac
dan <danphi@gmail.com>
2015-03-18 22:16:24 -0700

    profiles

--- base/server/connection.js
+++ base/server/connection.js
@@ -3,7 +3,9 @@ landingConnection = DDP.connect(process.env.DOMINUS_BASE);
 
 callLandingMethod = function(methodName, email, options) {
     if (landingConnection.status().connected) {
-        landingConnection.call(methodName, process.env.DOMINUS_KEY, email, process.env.GAME_ID, options);
+        Meteor.defer(function() {
+            landingConnection.call(methodName, process.env.DOMINUS_KEY, email, process.env.GAME_ID, options);
+        });
     } else {
         console.error('no connection to base');
     }
