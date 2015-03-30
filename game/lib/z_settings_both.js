f2d630bae91d8ef713c7f09a9d303813daf6f664
dan <danphi@gmail.com>
2015-03-29 22:08:48 -0700

    only hold dominus for two days to win

--- game/lib/z_settings_both.js
+++ game/lib/z_settings_both.js
@@ -2,4 +2,7 @@
 
 if (Meteor.settings.public.GAME_ID == 'dev') {
     s.serverMaxPlayers = 200;
+
+    // length of time added to game end clock when there is a new dominus
+    s.time_til_game_end_when_new_dominus = 1000 * 60 * 60 * 24 * 2; 	// 2 days
 }
