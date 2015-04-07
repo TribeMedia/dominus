178227867a3617b303e465ebbdd4437d9a685466
dan <danphi@gmail.com>
2015-04-07 09:27:17 -0700

    trying different ads

---
+++ game/miscTemplates/adSquare/client/adSquare.js
@@ -0,0 +1,10 @@
+Template.adSquare.onRendered(function() {
+    // var adsbygoogle;
+    // (adsbygoogle = window.adsbygoogle || []).push({});
+
+    $.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
+        var ads, adsbygoogle;ads = '<ins class="adsbygoogle" style="display:inline-block;width:250px;height:250px" data-ad-client="ca-pub-3932000594707687" data-ad-slot="6768509647"></ins>';
+        $('#adSquare').html(ads);
+        return (adsbygoogle = window.adsbygoogle || []).push({});
+    });
+});
--- game/miscTemplates/rightAdPanel/client/rightAdPanel.js
+++ game/miscTemplates/rightAdPanel/client/rightAdPanel.js
@@ -1,4 +1,10 @@
 Template.rightAdPanel.onRendered(function() {
-    var adsbygoogle;
-    (adsbygoogle = window.adsbygoogle || []).push({});
+    // var adsbygoogle;
+    // (adsbygoogle = window.adsbygoogle || []).push({});
+
+    $.getScript("//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", function() {
+        var ads, adsbygoogle;ads = '<ins class="adsbygoogle" style="display:inline-block;width:160px;height:600px" data-ad-client="ca-pub-3932000594707687" data-ad-slot="6908110447"></ins>';
+        $('#rightAdPanel').html(ads);
+        return (adsbygoogle = window.adsbygoogle || []).push({});
+    });
 });
